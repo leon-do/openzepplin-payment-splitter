cd7498e3141b25806c003e44d822ac68fc112cf5
leon-do <leondo123@gmail.com>
2022-04-04 23:44:10 -0400

    add .github workflow

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  assetPrefix: isProd ? '/openzepplin-payment-splitter' : '',
   reactStrictMode: true,
 }
 
